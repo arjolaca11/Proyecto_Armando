fe978c7152e11ee015edb998bc7140ca5d6d9741
arjolaca11 <145235517+arjolaca11@users.noreply.github.com>
2025-08-27 02:01:08 -0600

    Add files via upload

---
+++ pruebasexamen/Amigo.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebasexamen;
+
+/**
+ *
+ * @author arjol
+ */
+//Representa a una persona del grupo.
+ //Guarda el nombre y un saldo acumulado
+ 
+public class Amigo {
+    private String nombre;
+    private double saldo;
+
+    // Constructor vacío )
+    public Amigo() {
+        this.nombre = "";
+        this.saldo = 0;
+    }
+
+    // Constructor principal
+    public Amigo(String nombre) {
+        this.nombre = nombre;
+        this.saldo = 0;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Setters 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    // Ajusta el saldo (puede ser positivo o negativo)
+    public void ajustarSaldo(double monto) {
+        this.saldo = this.saldo + monto;
+    }
+ 
+}
